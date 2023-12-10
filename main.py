aa03a60b75ad9ae849c0d21f539911d9c82b11fc
Liopol2 <leonardosoumas3@gmail.com>
2023-12-10 19:36:55 -0300

    refactored a bit

--- main.py
+++ main.py
@@ -1,66 +1,63 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time,calendar
 from datetime import datetime
-import pandas as pd
+#import pandas as pd
 
 driver_path= "C:\\Users\\leong\\Downloads\\chromedriver.exe"
 
 options = webdriver.ChromeOptions()
 options.add_argument('--start-maximized')
 options.add_argument('--disable-extensions')
+inicio=datetime.now().second
 hoy = datetime.now().day
 esteMes = datetime.now().month
 esteaño = datetime.now().year
 #la primer variable es el primer dia habil del mes, ese nombre es porque no lo pienso usar
 _,diasenmes = calendar.monthrange(esteaño, esteMes)
-valores=dict()
+compra=''
+venta=''
 
 def cargarpagina():
     driver = webdriver.Chrome(options=options)
-    time.sleep(1)
-    driver.get("https://www.brou.com.uy/cotizaciones")
-    WebDriverWait(driver,3)    
+    driver.get("https://www.brou.com.uy/cotizaciones")        
     fetchValores(driver)
     driver.quit()
+    #print('valores',datetime.now().second - inicio)
 
 def fetchValores(driver):
     #compra
-    dolar_compra=driver.find_element(By.XPATH,'/html/body/div[2]/div[5]/div/div/div/div/div[1]/div/div/div/table/tbody/tr[1]/td[3]/div/p')
+    dolar_compra=WebDriverWait(driver,3).until(EC.presence_of_element_located(
+            (By.XPATH,'/html/body/div[2]/div[5]/div/div/div/div/div[1]/div/div/div/table/tbody/tr[1]/td[3]/div/p')))
     dolar_compra=dolar_compra.text[0:5]
     dolar_compra=dolar_compra.replace(',','.')
     floatdolar_compra=float(dolar_compra)
-    print('Dolar compra: ',dolar_compra)
-    valores['compra']=dolar_compra
+    venta=dolar_compra
 
     #venta
-    dolar_venta= driver.find_element(By.XPATH,'/html/body/div[2]/div[5]/div/div/div/div/div[1]/div/div/div/table/tbody/tr[2]/td[3]/div/p')
+    dolar_venta=WebDriverWait(driver,3).until(EC.presence_of_element_located((
+            By.XPATH,'/html/body/div[2]/div[5]/div/div/div/div/div[1]/div/div/div/table/tbody/tr[2]/td[3]/div/p')))
     dolar_venta=dolar_venta.text[0:5]
     dolar_venta=dolar_venta.replace(',','.')
     floatdolar_venta=float(dolar_venta)
-    print('Dolar venta: ',dolar_venta)
-    valores['venta']=dolar_venta
+    venta=dolar_venta
 
 def agregarATabla(compra,venta):
-    fecha=datetime.today().strftime('%d-%m-%Y') 
-    try:
-        open('dolar.csv','r')
-    except(IOError):
-        tabla=open('dolar.csv','a')
-        tabla.write('Fecha, Dolar Compra, Dolar Venta')
-        print('se creo la tabla de valores')
-    else:
-        tabla = open('dolar.csv',"a")
-    tabla.write('\n')
-    tabla.write(fecha + " , ")
-    tabla.write(str(compra) + " , ")
-    tabla.write(str(venta))
-    tabla.close()
+   fecha=datetime.today().strftime('%d-%m-%Y') 
+   with open('dolar.csv','a+') as tabla:
+       if tabla.tell() == 0:
+           tabla.write('Fecha, Dolar Compra, Dolar Venta\n')
+       tabla.write(fecha + " , ")
+       tabla.write(compra + " , ")
+       tabla.write(venta + "\n")
+       tabla.close()    
+   #print("tabla ", datetime.now().second - inicio)
 
 while hoy < diasenmes :
     cargarpagina()
-    agregarATabla( valores['compra'],valores['venta'])
+    agregarATabla(compra,venta)
     hoy += 1
     print("esperando hasta mañana, hoy es: ", hoy)    
     time.sleep(86400)
